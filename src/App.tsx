58c96ea470975e70e4e8a00a8e34775fb998fc23
raphaelpg <raphael_pinto@outlook.com>
2023-03-07 15:36:51 +0100

    add init ton hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
+import { useCounterContract } from './hooks/useCounterContract';
 
 function App() {
+  const { value, address } = useCounterContract();
+
   return (
-    <div>
-      <TonConnectButton />
+    <div className='App'>
+      <div className='Container'>
+        <TonConnectButton />
+
+        <div className='Card'>
+          <b>Counter Address</b>
+          <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
+        </div>
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{value ?? 'Loading...'}</div>
+        </div>
+      </div>
     </div>
   );
 }

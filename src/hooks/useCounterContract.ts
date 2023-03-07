8866ab69e03fd7d6f7d6ac78adefe3c74568339b
raphaelpg <raphael_pinto@outlook.com>
2023-03-07 15:48:41 +0100

    add increment button

--- src/hooks/useCounterContract.ts
+++ src/hooks/useCounterContract.ts
@@ -2,11 +2,15 @@ import { useEffect, useState } from 'react';
 import Counter from '../contracts/counter';
 import { useTonClient } from './useTonClient';
 import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
 import { Address, OpenedContract } from 'ton-core';
 
 export function useCounterContract() {
   const client = useTonClient();
-  const [val, setVal] = useState<null | number>();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
   const counterContract = useAsyncInitialize(async () => {
     if (!client) return;
@@ -21,7 +25,9 @@ export function useCounterContract() {
       if (!counterContract) return;
       setVal(null);
       const val = await counterContract.getCounter();
-      setVal(Number(val));
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
     }
     getValue();
   }, [counterContract]);
@@ -29,5 +35,8 @@ export function useCounterContract() {
   return {
     value: val,
     address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
   };
 }

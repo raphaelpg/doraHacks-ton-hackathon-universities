13480d131e5a17194e2588c1b805b36d9b93d421
raphaelpg <raphael_pinto@outlook.com>
2023-03-13 23:22:17 +0100

    WIP user factory address getter

---
+++ university-contracts/tests/Main.spec.ts
@@ -0,0 +1,95 @@
+import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Address, Cell, toNano } from 'ton-core';
+import { Main } from '../wrappers/Main';
+import '@ton-community/test-utils';
+import { compile } from '@ton-community/blueprint';
+import { randomAddress } from './helpers';
+
+describe('Main', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('Main');
+    });
+
+    let blockchain: Blockchain;
+    let main: SandboxContract<Main>;
+    let deployerAddress: Address;
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        const deployer = await blockchain.treasury('deployer');
+        deployerAddress = deployer.address;
+
+        main = blockchain.openContract(
+            Main.createFromConfig(
+                {
+                    owner_address: deployer.address,
+                    last_sender_address: deployer.address,
+                },
+                code
+            )
+        );
+
+        const deployResult = await main.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: main.address,
+            deploy: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and collectionFactory are ready to use
+    });
+
+    it('should retrieve owner address', async () => {
+        const owner = await main.getOwner();
+
+        console.log({owner})
+        console.log({"deployer.address": deployerAddress})
+
+        expect(owner.equals(deployerAddress)).toEqual(true);
+    })
+
+    it('should retrieve last sender address and it should be the deployer', async () => {
+        const lastSender = await main.getLastSender();
+
+        console.log({lastSender})
+        console.log({deployerAddress})
+        expect(lastSender.equals(deployerAddress)).toEqual(true);
+    })
+
+    it('should change last sender', async () => {
+        const newSender = await blockchain.treasury('newSender');
+
+        const lastSenderBefore = await main.getLastSender();
+
+        console.log('lastSenderBefore', lastSenderBefore);
+
+        await main.sendCreateCollection(newSender.getSender(), {
+            value: toNano('0.05'),
+        });
+
+        const lastSenderAfter = await main.getLastSender();
+
+        console.log('lastSenderAfter', lastSenderAfter);
+
+        expect(lastSenderBefore.equals(lastSenderAfter)).toEqual(false);
+    });
+
+    it("should retrieve user's factory", async () => {
+        const newSender = await blockchain.treasury('newSender');
+        await main.sendCreateCollection(newSender.getSender(), {
+            value: toNano('0.05'),
+        });
+
+        const userFactory = await main.getWalletFactoryAddress();
+
+        console.log({userFactory})
+        // console.log({deployerAddress})
+        // expect(lastSender.equals(deployerAddress)).toEqual(true);
+    })
+});
---
+++ university-contracts/wrappers/Main.ts
@@ -0,0 +1,97 @@
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+
+export type MainConfig = {
+    owner_address: Address;
+    last_sender_address: Address;
+};
+
+export function mainConfigToCell(config: MainConfig): Cell {
+    return beginCell()
+        .storeAddress(config.owner_address)
+        .storeAddress(config.last_sender_address)
+        .endCell();
+}
+
+export const Opcodes = {
+    transfer_ownership: 0x2da38aaf,
+    create_collection: 42,
+};
+
+export class Main implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new Main(address);
+    }
+
+    static createFromConfig(config: MainConfig, code: Cell, workchain = 0) {
+        const data = mainConfigToCell(config);
+        const init = { code, data };
+        return new Main(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendCreateCollection(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint;
+            queryID?: number;
+        }
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.create_collection, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .endCell(),
+        });
+    }
+
+    
+    async sendtransferOwnership(
+        provider: ContractProvider, 
+        via: Sender, 
+        opts: {
+            newOwnerAddress: Address;
+            value: bigint;
+            queryID?: number;
+        }
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.transfer_ownership, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .storeAddress(opts.newOwnerAddress)
+                .endCell(),
+        });
+    }
+
+    async getOwner(provider: ContractProvider) {
+        const result = await provider.get('get_owner_address', []);
+        return result.stack.readAddress();
+    }
+
+    async getLastSender(provider: ContractProvider) {
+        const result = await provider.get('get_last_sender_address', []);
+        return result.stack.readAddress();
+    }
+
+    async getWalletFactoryAddress(provider: ContractProvider, wallet: Address) {
+        const result = await provider.get('get_wallet_factory_address', wallet);
+        console.log({result})
+        console.log(result.stack)
+        console.log(result.stack.readAddress())
+        return result.stack.readAddress()
+    }
+}
